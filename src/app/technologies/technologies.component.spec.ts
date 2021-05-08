4db383bfc5e2cd47847a29b080c7f77b18894dea
lah L Ntumsi <lah_ntumsi2000@yahoo.com>
2021-05-07 23:21:15 -0500

    upgrade to angular 11

--- src/app/technologies/technologies.component.spec.ts
+++ src/app/technologies/technologies.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TechnologiesComponent } from './technologies.component';
 
@@ -6,7 +6,7 @@ describe('TechnologiesComponent', () => {
   let component: TechnologiesComponent;
   let fixture: ComponentFixture<TechnologiesComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TechnologiesComponent ]
     })
