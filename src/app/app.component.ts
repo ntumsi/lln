a9880cb96f1aea1b7ee2d23e45656028dc7e00f6
lah ntumsi <Lah Ntumsi>
2021-07-01 23:37:37 -0500

    updated the technologies component and projects component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+const d=new Date()
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lln';
+  year = d.getFullYear()
 }
