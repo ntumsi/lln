8a34a7e1cc0933a3c8de067f038ed5851a847547
lah L Ntumsi <lah_ntumsi2000@yahoo.com>
2021-08-23 19:39:19 -0500

    added configjs

---
+++ config.js
@@ -0,0 +1,10 @@
+window.config = {
+    cognito :{
+        userPoolId:'us-east-1_LjxccBWVX',
+        userPoolClientId:'4ja4iojntfdsm6kggbjqt5tp6h',
+        region: 'us-eat-1'
+    },
+    api: {
+        invokeUrl: 'portfolio.auth.us-east-1.amazoncognito.com'
+    }
+}
